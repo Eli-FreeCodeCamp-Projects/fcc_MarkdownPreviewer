c780cca9278f3c78aebe8424e79593bd1fdfb060
Eli <eli.serra173@gmail.com>
2023-10-06 04:48:06 +0200

    major update on resizableHelper.js:  - move classes on separate files.  - correct bugs  - improve code

--- src/utils/resizableHelper.js
+++ src/utils/resizableHelper.js
@@ -1,418 +1,15 @@
 import {DataStorage} from './dataStorage.js'
 import {ut} from './utils.js'
-/**
- * ResizableBase class
- */
-class ResizableBase{
-
-    COMBO_PANE = 'COMBO_PANE'
-    LEFT_PANE = 'LEFT_PANE'
-    RIGHT_PANE = 'RIGHT_PANE'
-
-    /**
-     *
-     * @param selector
-     * @param defaultValue
-     * @return {*|null}
-     */
-    static setSelector(selector, defaultValue= null){
-        const def = (ut.isSelector(defaultValue)) ? defaultValue : null;
-        return (ut.isSelector(selector)) ? selector : def;
-    }
-
-    /**
-     *
-     * @param value
-     * @return {*|boolean}
-     */
-    static isDimValue(value){
-        return ut.isNumber(value) && value >= 0;
-    }
-
-    /**
-     *
-     * @param value
-     * @param defaultValue
-     * @return {*|null}
-     */
-    static setDimValue(value, defaultValue= null){
-        const def = (ResizableBase.isDimValue(defaultValue)) ? defaultValue : null;
-        return (ResizableBase.isDimValue(value)) ? value : def;
-    }
-
-    /**
-     *
-     * @param selector
-     * @return {number|null}
-     */
-    getOffsetWith(selector){
-        const element = document.querySelector(selector)
-        return (ut.isElement(element)) ? element.offsetWidth : null;
-    }
-
-    /**
-     *
-     * @param selector
-     * @return {number|null}
-     */
-    getOffsetLeft(selector){
-        const element = document.querySelector(selector)
-        return (ut.isElement(element)) ? element.offsetLeft : null;
-    }
-}
-/**
- * SidePane class
- */
-class SidePane extends ResizableBase{
-    constructor(props){
-        super();
-        this.selector = null;
-        this.width = null;
-        this.minWidth = 300;
-        this.setProps(props);
-    }
-
-    /**
-     *
-     * @param props
-     */
-    setProps(props){
-        if(ut.isObject(props)){
-            this.setSelector(props.selector)
-            this.setMinWidth(props.minWidth);
-        }
-    }
-
-    /**
-     *
-     * @return {boolean|*}
-     */
-    isReady(){
-        return ut.isSelector(this.selector)
-    }
-
-    /**
-     *
-     * @param value
-     */
-    setMinWidth(value){
-        this.minWidth = SidePane.setDimValue(value, 300);
-    }
-
-    /**
-     *
-     * @param value
-     */
-    setSelector(value){
-        this.selector = SidePane.setSelector(value);
-    }
-
-    getElement(){
-        return document.querySelector(this.selector);
-    }
-
-    /**
-     *
-     * @param width
-     * @return {{visibility: string, width: string}|{visibility: string}|null}
-     */
-    static getPaneStyle(width){
-        if(ut.isPositiveNumber(width)){
-            return {
-                width: `${width}%`,
-                visibility: 'visible'
-            }
-        }else if(ut.isNumber(width) && width === 0){
-            return {
-                visibility: 'hidden'
-            }
-        }else{
-            return null;
-        }
-
-    }
-
-    /**
-     *
-     * @param width
-     * @return {*|boolean}
-     */
-    static isWidth(width){
-        return SidePane.isDimValue(width);
-    }
-
-    /**
-     *
-     * @param width
-     * @return {boolean}
-     */
-    setWidth(width){
-        if(SidePane.isWidth(width)){
-            this.width = width
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     *
-     * @return {{visibility: string, width: string}|{visibility: string}}
-     */
-    getStyle(){
-        return SidePane.getPaneStyle(this.width);
-    }
-
-    /**
-     *
-     * @return {null}
-     */
-    resizePane(width){
-        const element = this.getElement();
-        if(ut.isPositiveNumber(width)){
-            element.style.visibility = '';
-            element.style.width = `${width}%`;
-        }else{
-            element.style.width = `${0}`;
-            element.style.visibility = `hidden`;
-        }
-    }
-
-    /**
-     *
-     */
-    resetStyle(){
-        const element = this.getElement()
-        element.style.width = '';
-        element.style.visibility = '';
-        element.style.height = '';
-        element.style.display = '';
-    }
-}
+import ResizableBase from "./resizableBase.js";
+import ResizableWindow from "./resizableWindow.js";
+import SidePane from "./resizableSidePane.js";
+import ResizableElement from "./ResizableElement.js";
+import PreviewerNav from "./resizableNav.js";
+import {Unit, Display} from "./resizableEnums.js";
 
 /**
- * ResizeBar class
+ *
  */
-class ResizeBar extends ResizableBase{
-    constructor(props){
-        super()
-        this.selector = null;
-        this.left = null;
-
-        this.setProps(props);
-    }
-
-    setSelector(value){
-        this.selector = ResizeBar.setSelector(value);
-    }
-
-    getElement(){
-        return document.querySelector(this.selector);
-    }
-
-    setProps(props){
-        if(ut.isObject(props)){
-            this.setSelector(props.selector);
-        }
-    }
-
-    isReady(){
-        return ut.isSelector(this.selector)
-    }
-
-    isLeftValue(left){
-        return ResizeBar.isDimValue(left);
-    }
-    setLeft(left){
-        if(ut.isNumber(left)){
-            this.left = left;
-            return true;
-        }
-        return false;
-    }
-
-    getStyle(){
-        if(this.isLeftValue(this.left)){
-            return {
-                left: `${this.left}px`
-            };
-        }
-        return null;
-    }
-
-    setLeftPosition(){
-        if(this.isLeftValue(this.left)){
-            const element = this.getElement()
-            element.style.left = `${this.left}px`;
-            return true;
-        }
-        return null;
-    }
-
-    resetStyle(){
-        document.querySelector(this.selector).style.left = '';
-    }
-}
-
-/**
- * PreviewerNav class
- */
-class PreviewerNav extends ResizableBase{
-
-    constructor(props){
-        super();
-        this.btLeft = null;
-        this.btCombo = null;
-        this.btRight = null;
-        this.status = this.COMBO_PANE
-
-        this.setProps(props);
-    }
-
-    setBtLeft(value){
-        this.btLeft = PreviewerNav.setSelector(value);
-    }
-
-    setBtCombo(value){
-        this.btCombo = PreviewerNav.setSelector(value);
-    }
-
-    setBtRight(value){
-        this.btRight = PreviewerNav.setSelector(value);
-    }
-
-    isNavStatus(value){
-        return value === this.LEFT_PANE || this.COMBO_PANE  || this.RIGHT_PANE
-    }
-
-    setNavStatus(value){
-        this.status = (this.isNavStatus(value)) ? value : this.COMBO_PANE;
-    }
-
-    setProps(props){
-        if(ut.isObject(props)){
-            this.setBtLeft(props.btLeft);
-            this.setBtCombo(props.btCombo);
-            this.setBtRight(props.btRight);
-            this.setNavStatus(props.status);
-        }
-    }
-
-    initFromStatus(){
-        switch (this.status){
-            case this.LEFT_PANE:
-                this.setBtLeftActive();
-                break;
-            case this.COMBO_PANE:
-                this.setBtComboActive();
-                break;
-            case this.RIGHT_PANE:
-                this.setBtRightActive();
-                break;
-        }
-    }
-
-    isReady(){
-        return ut.isSelector(this.btLeft)
-            && ut.isSelector(this.btCombo)
-            && ut.isSelector(this.btRight)
-            && this.isNavStatus(this.status)
-    }
-
-    resetNavActive(){
-        document.querySelector(this.btLeft).classList.remove('active');
-        document.querySelector(this.btCombo).classList.remove('active');
-        document.querySelector(this.btRight).classList.remove('active');
-    }
-
-    isBtLeftActive(){
-        return document.querySelector(this.btLeft).classList.contains('active');
-    }
-
-    setBtLeftActive(){
-        if(!this.isBtLeftActive()) {
-            this.resetNavActive();
-            this.status = this.LEFT_PANE;
-            document.querySelector(this.btLeft).classList.add('active');
-        }
-    }
-
-    isBtComboActive(){
-        return document.querySelector(this.btCombo).classList.contains('active');
-    }
-
-    setBtComboActive(){
-        if(!this.isBtComboActive()) {
-            this.resetNavActive();
-            this.status = this.COMBO_PANE;
-            document.querySelector(this.btCombo).classList.add('active');
-        }
-    }
-
-    isBtRightActive(){
-        return document.querySelector(this.btRight).classList.contains('active');
-    }
-
-    setBtRightActive(){
-        if(!this.isBtRightActive()){
-            this.resetNavActive();
-            this.status = this.RIGHT_PANE;
-            document.querySelector(this.btRight).classList.add('active');
-        }
-
-    }
-}
-
-/**
- * PreviewerWindow class
- */
-class PreviewerWindow extends ResizableBase{
-    constructor(props){
-        super();
-        this.minWidth = null;
-        this.isWide = true;
-        this.resizeStarted = false;
-        this.mouseLeft = 0
-        this.width = 0
-
-        this.setProps(props);
-    }
-
-    setProps(props){
-        if(ut.isObject(props)){
-            this.setMinWidth(props.minWidth);
-        }
-    }
-
-    setMinWidth(value){
-        this.minWidth = PreviewerWindow.setDimValue(value);
-    }
-
-    isReady(){
-        return PreviewerWindow.isDimValue(this.minWidth)
-    }
-
-    isWideWindow(screenWidth){
-        return (
-            ut.isPositiveNumber(screenWidth)
-            && screenWidth >= this.minWidth
-        )
-    }
-
-    setTypeWindow(){
-        this.isWide = this.isWideWindow(window.innerWidth);
-        return this.isWide;
-    }
-
-    setMouseLeft(mouseLeft){
-        this.mouseLeft = (ut.isPositiveNumber(mouseLeft) ? mouseLeft : null);
-    }
-}
-
-/**
- * PreviewerHelper class
- * This class is used to resize previewer windows.
- */
-
 export class ResizableHelper extends ResizableBase{
     storageKey = null;
     window = null;
@@ -432,7 +29,7 @@ export class ResizableHelper extends ResizableBase{
      * @param props {{minWidth: Number}} The min with of window to limit resize.
      */
     setWindow(props){
-        this.window = new PreviewerWindow(props);
+        this.window = new ResizableWindow(props);
     }
 
     /**
@@ -444,18 +41,34 @@ export class ResizableHelper extends ResizableBase{
         this.nav = new PreviewerNav(props);
     }
 
+    /**
+     *
+     * @param props
+     */
     setLeftPane(props){
         this.leftPane = new SidePane(props);
     }
 
+    /**
+     *
+     * @param props
+     */
     setRightPane(props){
         this.rightPane = new SidePane(props);
     }
 
+    /**
+     *
+     * @param props
+     */
     setResizeBar(props){
-        this.resizeBar = new ResizeBar(props);
+        this.resizeBar = new ResizableElement(props);
     }
 
+    /**
+     *
+     * @param props
+     */
     setProps(props){
         if(ut.isObject(props)){
             this.setWindow(props.window);
@@ -478,6 +91,9 @@ export class ResizableHelper extends ResizableBase{
             && this.resizeBar.isReady()
     }
 
+    /**
+     *
+     */
     getDataFromStorage(){
         const storageData = DataStorage.getStoreData(this.storageKey);
         if(ut.isObject(storageData)){
@@ -497,7 +113,6 @@ export class ResizableHelper extends ResizableBase{
         return {
             left_w: this.leftPane.width,
             right_w: this.rightPane.width,
-            resizeBarLeft: this.resizeBar.left,
             nav: this.nav.status,
         }
     }
@@ -505,72 +120,65 @@ export class ResizableHelper extends ResizableBase{
     getPanesSize(){
         return {
             left_w: this.leftPane.width,
-            right_w: this.rightPane.width,
-            resizeBarLeft: this.resizeBar.left,
+            right_w: this.rightPane.width
         }
     }
 
-    setPanesSize(size){
-        const {left_w, right_w, resizeBarLeft} = size;
+    getPixToPercentSize(size){
+        const {left_w, right_w} = size;
+        return {
+            left_w: this.window.getPixToPercent(left_w),
+            right_w: this.window.getPixToPercent(right_w)
+        }
+    }
+
+    getPercentSizes(size, unit){
+        if(unit === Unit.PIXEL){
+            return this.getPixToPercentSize(size);
+        }
+        return size;
+    }
+
+    setPanesSize(size, unit= Unit.PERCENT){
+        const {left_w, right_w} = this.getPercentSizes(size, unit);
         this.leftPane.setWidth(left_w);
         this.rightPane.setWidth(right_w);
-        this.resizeBar.setLeft(resizeBarLeft);
     }
 
-    resizePanes(size){
-        const {left_w, right_w, resizeBarLeft} = size;
+    resizePanes(size, unit=Unit.PERCENT){
+        const {left_w, right_w} = this.getPercentSizes(size, unit);
         this.leftPane.resizePane(left_w);
         this.rightPane.resizePane(right_w);
-        this.resizeBar.setLeftPosition(resizeBarLeft);
     }
 
-    setAndResizePanes(size){
-        this.setPanesSize(size)
-        this.resizePanes(size)
-    }
-
-    getOffsetWith(element){
-        return (ut.isElement(element)) ? element.offsetWidth : null;
-    }
-
-    getOffsetLeft(element){
-        return (ut.isElement(element)) ? element.offsetLeft : null;
+    setAndResizePanes(size, unit=Unit.PERCENT){
+        this.setPanesSize(size, unit)
+        this.resizePanes(size, unit)
     }
 
     resetWindowsSizes(){
         this.leftPane.resetStyle();
         this.rightPane.resetStyle();
-        this.resizeBar.resetStyle();
     }
 
     handleResizeWindow(){
         const mouseX = this.window.mouseLeft,
             left_pane = this.leftPane.getElement(),
             left_x = this.getOffsetLeft(left_pane),
-            right_x = this.getOffsetLeft(this.rightPane.getElement()),
-            right_w = this.getOffsetWith(this.resizeBar.getElement()),
-            window_w = window.innerWidth,
+            bar_w = this.resizeBar.getWidth(),
+            window_w = this.window.getWidth(),
             is_min_left = (mouseX >= this.leftPane.minWidth),
             is_min_right = (mouseX <= window_w - this.rightPane.minWidth);
 
         if(is_min_left && is_min_right ){
-            // Get container width
-            const container = left_pane.parentElement,
-                container_width = this.getOffsetWith(container);
+
             // Get width of left and right panes in pixel
-
             const left_px = (mouseX - left_x),
-                right_px = ((right_x + right_w) - mouseX);
-
-            // and Transform width values in %
-            const left_width = ut.toFixedFloat(left_px * 100 / container_width),
-                right_width = ut.toFixedFloat(right_px * 100 / container_width);
-
+                right_px = window_w - left_px - bar_w;
             this.setAndResizePanes({
-                left_w: left_width,
-                right_w:  right_width,
-                resizeBarLeft: mouseX
-            });
+                left_w: left_px,
+                right_w:  right_px
+            }, Unit.PIXEL);
         }
     }
 
@@ -599,19 +207,13 @@ export class ResizableHelper extends ResizableBase{
         return this.rightPane.getStyle();
     }
 
-    getResizeBarStyle(){
-        return this.resizeBar.getStyle();
-    }
-
     expandLeftPane(){
         this.nav.setBtLeftActive();
         this.resetWindowsSizes();
         if(this.window.isWide){
-            const windowWidth = window.innerWidth;
             this.resizePanes({
-                left_w: windowWidth,
-                right_w:  0,
-                resizeBarLeft: windowWidth
+                left_w: 100,
+                right_w:  0
             });
         }else{
             const left_pane = this.leftPane.getElement(),
@@ -629,8 +231,7 @@ export class ResizableHelper extends ResizableBase{
         if(this.window.isWide){
             this.resizePanes({
                 left_w: 0,
-                right_w:  window.innerWidth,
-                resizeBarLeft: 0
+                right_w:  100
             });
         }else{
             const left_pane = this.leftPane.getElement(),
@@ -644,7 +245,6 @@ export class ResizableHelper extends ResizableBase{
     hasComboDataView(){
         return ut.isPositiveNumber(this.leftPane.width)
             && ut.isPositiveNumber(this.rightPane.width)
-            && ut.isPositiveNumber(this.resizeBar.left)
     }
 
     loadComboView(){
@@ -656,17 +256,15 @@ export class ResizableHelper extends ResizableBase{
                 this.resizePanes(this.getPanesSize());
 
             }else{
-                const windowWidth = parseInt(window.innerWidth/2);
                 this.setAndResizePanes({
-                    left_w: windowWidth,
-                    right_w:  windowWidth,
-                    resizeBarLeft: windowWidth
+                    left_w: 50,
+                    right_w:  50
                 });
             }
 
         }else{
             switch(this.nav.status){
-                case this.RIGHT_PANE:
+                case Display.RIGHT_PANE:
                     this.expandRightPane();
                     break;
                 default:
@@ -678,13 +276,13 @@ export class ResizableHelper extends ResizableBase{
 
     loadView(){
         switch(this.nav.status){
-            case this.LEFT_PANE:
+            case Display.LEFT_PANE:
                 this.expandLeftPane();
                 break;
-            case this.RIGHT_PANE:
+            case Display.RIGHT_PANE:
                 this.expandRightPane();
                 break;
-            case this.COMBO_PANE:
+            case Display.COMBO_PANE:
                 this.loadComboView();
                 break;
             default:
@@ -707,10 +305,6 @@ export class ResizableHelper extends ResizableBase{
     ExpandWindow(element){
         if(ut.isElement(element)){
             const controls = element.getAttribute('aria-controls');
-            const is_wide = this.window.isWide
-            if(this.window.setTypeWindow() !== is_wide){
-                this.resetWindowsSizes();
-            }
             switch(controls){
                 case 'm8_left_body':
                     this.expandLeftPane();
